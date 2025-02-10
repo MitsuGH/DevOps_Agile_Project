ef086776e2515c5957b81d3c1f24fa09d8c89413
choneyseldon <153059121+choneyseldon@users.noreply.github.com>
2025-02-11 02:20:09 +0700

    home-trail page

---
+++ auth-app/public/js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.upcoming i').addEventListener('click', function() {
+    var submenu = document.querySelector('.submenu');
+    submenu.classList.toggle('show');
+    submenu.classList.toggle('hidden');
+});
